3071dbddf8296e50b9d5ff5e6bb02fa0d3551354
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-07-08 15:23:17 +0700

    Thinh_8/7/2024-best coder

--- PhatSinhBaiGiang/Assistant_Server/app.js
+++ PhatSinhBaiGiang/Assistant_Server/app.js
@@ -1,50 +1,57 @@
+// Import required modules
 const express = require('express');
 const cors = require('cors');
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
+// Load environment variables
 require('dotenv').config();
 
+// Create an instance of GoogleGenerativeAI
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
+// Create an Express app
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(cors({
     origin: '*',
     optionsSuccessStatus: 200,
 }));
 
+const prompt = 
 
+// Route to get Gemini answer
 app.post('/api/getGeminiAnswer', async (req, res, next) => {
     try {
         const { question, gemini_model } = req.body;
-        if(question != "") {
-            const model = genAI.getGenerativeModel({ model: gemini_model});
+        if (question != "") {
+            const model = genAI.getGenerativeModel({ model: gemini_model });
 
             const result = await model.generateContentStream([question]);
-            for await(const chunk of result.stream){
+            for await (const chunk of result.stream) {
                 const chunkText = chunk.text();
                 res.write(chunkText);
             }
-        }
-        else {
+        } else {
             res.write(err);
         }
         res.end();
-        
+
     } catch (error) {
-      return next(error)
-    }  
-    
-})
+        return next(error)
+    }
+});
 
+// Route to get weather data
 app.get('/api/trongnuoc', (req, res) => {
-    //Response
+    // Fetch weather data from external API
     fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY2}&q=Hanoi&aqi=no`)
-    .then(r => r.text())
-    .then(data => {
-        res.json(data);
-    });
-})
+        .then(r => r.text())
+        .then(data => {
+            res.json(data);
+        });
+});
 
-
+// Start the server
 app.listen(process.env.PORT);
